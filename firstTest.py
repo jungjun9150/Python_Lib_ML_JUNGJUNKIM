04502c02c5025e37c61c3661b3b1ccf449c45298
JUNGJUN KIM <jungjun9150@naver.com>
2018-02-11 19:00:49 -0800

    add the firstTest.py Condensation Excel file

--- firstTest.py
+++ firstTest.py
@@ -18,25 +18,27 @@ target_file_name = '/home/jungjunkim/Python_Library_Machine_Learning/Dataset/tar
 ## Creating numpy array from csv
 dataset=np.genfromtxt(dataset_file_name,delimiter=',')
 target=np.genfromtxt(target_file_name,delimiter=',')
+names =np.array(['InTemp','Inhumi','Otemp','Ohumi'])
 
-names =np.array(['temp','wspeed','wway','humi','hpa'])
 ## Spliting Train_data and Test_data
 X_train, X_test, y_train, y_test = train_test_split(dataset,target,random_state=0)
+
 
 ## Drawing the Scatter matrix plot
 Scatter_plot = pd.DataFrame(X_train,columns=names)
 pd.plotting.scatter_matrix(Scatter_plot,c=y_train,figsize=(15,15),marker='o',hist_kwds={'bins':20},s=60,alpha=.8,cmap=mglearn.cm3)
 #plt.show()
 
-## Creating Mode by K-Nearest Neighbors
+## Creating Model by K-Nearest Neighbors
 knn = KNeighborsClassifier(n_neighbors=1)
 knn.fit(X_train,y_train)
 
 ## Predicting the model
-#y_pred = knn.predict(X_test)
-print(format(X_train))
+y_pred = knn.predict(X_test)
+print("testset correction: {:.2f}".format(np.mean(y_pred == y_test)))
 
-
+print("test y_pred:\n{}".format(y_pred))
+print("test y_test:\n{}".format(y_test))
 
 #print(format(dataset.shape))
 #print(format(target.shape))
